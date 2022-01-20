5733877468d43c527e766ee7d3fa7df7d1d0b1a1
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-20 21:22:17 +0100

    Cleaned some code.

--- secrets.py
+++ secrets.py
@@ -11,6 +11,4 @@ class Data:
 
     __client_secret = '3d2a3a5a64884371b1b28207a0130017'
     __client_id = '7887fe78ed5d44e9bb95a7a901246b38'
-
-    __client_creds = f'{__client_id}:{__client_secret}'
-    client_creds_base64 = base64.b64encode(__client_creds.encode()).decode()
+    client_creds_base64 = base64.b64encode(f'{__client_id}:{__client_secret}'.encode()).decode()
