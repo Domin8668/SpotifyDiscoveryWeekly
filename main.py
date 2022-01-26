205f22b139a4b8a753150471465ba1136ed20c59
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2022-01-26 16:24:15 +0100

    Cleaned some code and added comments.

--- main.py
+++ main.py
@@ -11,77 +11,88 @@ class SaveSongs:
         self.spotify_token = "AQBtJO9gXoZEew79eoecggYcrSabg4igWaF7VRDGVbF2YaK3piATbsvfDlS9Jf6imOAP0APrwA6OIeSwx" \
                              "ypmOlGpFaYdZpD2wf_4hlpJRtM9zU4Zl0X-9rKyavDpLR7E5u4"
         self.discover_weekly_id = Data.discover_weekly_id
-        self.tracks = ""
-        self.new_playlist_id = ""
+        self.tracks = ''
+        self.new_playlist_id = ''
+        self.name = ''
 
-    def call_refresh(self):
+    def start(self):
         print("Refreshing token...")
         # Refreshing the authorization token.
         self.spotify_token = Refresh.refresh()
-        # Finding songs.
-        self.find_songs()
+        # Checking if the playlist already exists.
+        self.check_playlist()
 
-    def find_songs(self):
-
-        print("Finding songs in discover weekly...")
-        query = f'https://api.spotify.com/v1/playlists/{Data.discover_weekly_id}/tracks'
-        headers = {"Content-Type": "application/json",
-                   "Authorization": f'Bearer {self.spotify_token}'}
-        # Getting the playlist data.
-        response = requests.get(query, headers=headers)
-        print(f'{response=}')
-        response_json = response.json()
-
-        tracks_list = [i["track"]["uri"] for i in response_json["items"]]
-        self.tracks = ",".join(tracks_list)
-        print(f'{self.tracks}')
-
-        self.add_to_playlist()
-
-    def add_to_playlist(self):
-        print("Adding songs...")
-        # Creating a new playlist.
-        self.new_playlist_id = self.create_playlist()
-
-        query = f'https://api.spotify.com/v1/playlists/{self.new_playlist_id}/tracks?uris={self.tracks}'
-        headers = {"Content-Type": "application/json",
-                   "Authorization": f'Bearer {self.spotify_token}'}
-        response = requests.post(query, headers=headers)
-        print(f'{response.json=}')
-
-    def create_playlist(self):
-        # TODO
-        # Check if this playlist already exists
-        # and only create a new one if it doesn't exist.
-        print("Trying to create playlist...")
+    def check_playlist(self):
+        print("Checking if the playlist exists...")
+        # Creating the name.
         today = date.today()
         monday = today - timedelta(days=today.weekday())
-        monday = monday.strftime("%d/%m/%Y")
-        print(f'{monday=}')
-
-        name = f'Discover Weekly {monday}'
+        self.name = f'Discover Weekly {monday.strftime("%d/%m/%Y")}'
+        # Getting all the playlist followed by the user.
         query = f'https://api.spotify.com/v1/users/{Data.spotify_user_id}/playlists'
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f'Bearer {self.spotify_token}'
+        }
+        response = requests.get(query, headers=headers)
+        # Checking if the playlist already exists.
+        response_json = response.json()
+        for playlist in response_json["items"]:
+            if playlist["name"] == self.name:
+                print("This playlist already exists.")
+                return 0
+        self.create_playlist()
 
+    def create_playlist(self):
+        print("Trying to create playlist...")
+        # Creating a new playlist.
+        query = f'https://api.spotify.com/v1/users/{Data.spotify_user_id}/playlists'
         request_body = json.dumps({
-            "name": name,
+            "name": self.name,
             "description": "Discover weekly rescued once again from the brink of destruction "
                            "by your friendly neighbourhood python script", "public": True
         })
-
         headers = {
             "Content-Type": "application/json",
             "Authorization": f'Bearer {self.spotify_token}'
         }
         response = requests.post(query, data=request_body, headers=headers)
-
         response_json = response.json()
+        # Getting the playlist id.
+        self.new_playlist_id = response_json["id"]
+        self.find_songs()
 
-        return response_json["id"]
+    def find_songs(self):
+        # Finding all the songs to add.
+        print("Finding songs in discover weekly...")
+        query = f'https://api.spotify.com/v1/playlists/{Data.discover_weekly_id}/tracks'
+        headers = {"Content-Type": "application/json",
+                   "Authorization": f'Bearer {self.spotify_token}'}
+        response = requests.get(query, headers=headers)
+        response_json = response.json()
+        # Get all the songs from Discover Weekly.
+        tracks_list = [i["track"]["uri"] for i in response_json["items"]]
+        self.tracks = ",".join(tracks_list)
+        self.add_to_playlist()
+
+    def add_to_playlist(self):
+        # Adding songs to the new playlist.
+        print("Adding songs...")
+        query = f'https://api.spotify.com/v1/playlists/{self.new_playlist_id}/tracks?uris={self.tracks}'
+        headers = {"Content-Type": "application/json",
+                   "Authorization": f'Bearer {self.spotify_token}'}
+        response = requests.post(query, headers=headers)
+        if response.status_code == 201:
+            print("Songs were added to the playlsit!")
+            return 0
+        else:
+            print(f'There was a {response.status_code} error.')
+            return -1
 
 
 def main():
     s = SaveSongs()
-    s.call_refresh()
+    s.start()
 
 
 if __name__ == "__main__":
