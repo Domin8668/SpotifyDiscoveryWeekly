0f0a5d4c8607796a32113f3e8448fbe2c3e8b47e
Domin <58365481+Domin8668@users.noreply.github.com>
2022-01-30 11:36:07 +0100

    Updated secrets.py

--- secrets.py
+++ secrets.py
@@ -2,13 +2,12 @@ import base64
 
 
 class Data:
-    spotify_user_id = "domiins"
-    discover_weekly_id = "37i9dQZEVXcP6X8abxWKSF"
+    spotify_user_id = ""
+    discover_weekly_id = ""
 
-    refresh_token = "AQBtJO9gXoZEew79eoecggYcrSabg4igWaF7VRDGVbF2YaK3piATbsvfDlS9Jf6imOAP0APrwA6OIeSwxypm" \
-                    "OlGpFaYdZpD2wf_4hlpJRtM9zU4Zl0X-9rKyavDpLR7E5u4"
-    redirect_uri = "https%3A%2F%2Fsave.dw"
+    refresh_token = ""
+    redirect_uri = ""
 
-    __client_secret = '3d2a3a5a64884371b1b28207a0130017'
-    __client_id = '7887fe78ed5d44e9bb95a7a901246b38'
+    __client_secret = ''
+    __client_id = ''
     client_creds_base64 = base64.b64encode(f'{__client_id}:{__client_secret}'.encode()).decode()
